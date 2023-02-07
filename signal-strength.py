281a73673743b5c3c57d58c72f39996a58fd4d8f
su0su <63037424+su0su@users.noreply.github.com>
2023-02-07 23:43:51 +0900

    Add files via upload

---
+++ signal-strength.py
@@ -0,0 +1,24 @@
+from scapy.all import *
+
+class beacon:
+    def __init__(self, interface, mac):
+        self.interface= interface
+        self.mac = mac
+        self.rssi = None
+
+def packet_handler(pkt):
+    if pkt.haslayer(Dot11) and pkt.type==0 and pkt.subtype==8:
+            if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
+                if pkt.addr2 == b.mac.lower() :              
+                    try : 
+                        b.rssi = -(256-ord(pkt.notdecoded[-2:-1]
+                    except:
+                        b.rssi=-1000    
+                    print ("signal-strength={}".format(b.rssi))
+                
+if __name__ == "__main__":
+    inf=interface=sys.argv[1]
+    m=sys.argv[2]
+    b=beacon(inf, m)
+    sniff(iface=b.interface, prn=packet_handler)
+    
